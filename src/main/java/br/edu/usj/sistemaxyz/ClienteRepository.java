2002aecf86497b0e6e8f412559284e0b4b24fd03
Arthur Nunes Catarina <xarthur.nuness@gmail.com>
2020-07-01 20:22:15 -0300

    Commit para o Heroku

---
+++ src/main/java/br/edu/usj/sistemaxyz/ClienteRepository.java
@@ -0,0 +1,11 @@
+package br.edu.usj.sistemaxyz;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+    List<Cliente> findByNomeContainingIgnoreCase(String nome);
+    List<Cliente> findAll();
+    
+}
